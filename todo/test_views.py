0ee58d71cfa3d414a6685aa5d008f466594e1988
LukaGajic2 <>
2023-07-28 21:27:53 +0000

    Create tests to test models.py

--- todo/test_views.py
+++ todo/test_views.py
@@ -35,5 +35,13 @@ class TestViews(TestCase):
         item = Item.objects.create(name='Test Todo Item', done=True)
         response = self.client.get(f'/toggle/{item.id}')
         self.assertRedirects(response, '/')
-        updated_items = Item.objects.filter(id=item.id)
-        self.assertFalse(updated_items.done)
+        updated_item = Item.objects.get(id=item.id)
+        self.assertFalse(updated_item.done)
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(
+            f'/edit/{item.id}', {'name': 'Updated name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
